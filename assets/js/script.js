7afa7be2e112abfc9d79126c734ead8524415891
Plain Jane <jane99hsu@gmail.com>
2020-08-29 12:57:30 -0700

    created basic html and css and pseudocode js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Start screen gives the user a summary of the quiz and scoring
+
+
+// Question section
+
+
+// Game over section shows user's final score asks for user initials
+
+
+// High score section shows user's final score and initials in a list
+
