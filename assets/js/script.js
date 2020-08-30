ceb2fd8ef984fc9519872e9f93422dc602a204af
Plain Jane <jane99hsu@gmail.com>
2020-08-29 23:38:57 -0700

    added array of questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,6 @@ function countdown() {
     }, 1000);
 }
   
-countdown();
 
 // Start screen gives the user a summary of the quiz and scoring
 // Start screen requires user to click the "Start" button to proceed to questions
@@ -41,39 +40,102 @@ function displayStart() {
         Good luck!'
     ;
 
+    // this displays the message
     startMessageEl.innerHTML = message;
 
+    // this displays the start button
     var buttonEl = createStartButton();
     startButtonEl.appendChild(buttonEl);
+
+
 
 }
 
 var createStartButton = function() {
-    
+
     // this is creating the start button
     var startButtonEl = document.createElement("button");
     startButtonEl.textContent = "Start";
     startButtonEl.className = "start-btn";
-    // editButtonEl.setAttribute("data-task-id", taskId);
+    startButtonEl.setAttribute("id", 'start');
 
     return startButtonEl;
 
 }
 
-
-displayStart();
-
-
 // Question function
 // Questions are in an array.
-// Start with timer != 0 && TRUE
-// Keep track of the index and print current question.
-// Questions require user to click a button to respond.
-// If question is answered correctly (TRUE), goes to next question (++).
-// Else incorrectly (FALSE) answered question decreases time then goes to next question if time is not 0.
-// If timer = 0, returns out of function to game over function.
 
-var display
+function startQuiz() {
+
+    // these are the questions
+    var questions = [
+        {
+            q: 'What is the most common sequence of events for a triathlon?',
+            a: 'run, swim, bike',
+            b: 'swim, bike, run',
+            c: 'bike, run, swim',
+            d: 'run, bike, swim',
+            answer: 'b'
+        },
+        {
+            q: 'What is the distance run in a marathon?',
+            a: '13.1 miles',
+            b: '31.1 miles',
+            c: '26.2 miles',
+            d: '6.2 miles',
+            answer: 'c'
+        },
+        {
+            q: 'Who is the first woman to climb a route graded 5.15a?',
+            a: 'Sasha DiGiulian',
+            b: 'Alex Puccio',
+            c: 'Nina Williams',
+            d: 'Margo Hayes',
+            answer: 'd'
+        }
+        {
+            q: 'What sport is Danny MacAskill known for?',
+            a: 'Street trials',
+            b: 'Snowboarding',
+            c: 'Rock climbing',
+            d: 'Surfing',
+            answer: 'a'
+        }
+        {
+            q: 'Which of the following trails is the longest?',
+            a: 'Pacific Crest Trail',
+            b: 'Continental Divide Trail',
+            c: 'Appalachian Trail',
+            d: 'John Muir Trail',
+            answer: 'b'
+        }
+    ];
+
+    // Start with timer != 0 && TRUE
+    // Keep track of the index and print current question.
+    // Questions require user to click a button to respond.
+    // If question is answered correctly (TRUE), goes to next question (++).
+    // Else incorrectly (FALSE) answered question decreases time then goes to next question if time is not 0.
+    // If timer = 0, returns out of function to game over function.
+
+    // copied this from the class exercise.  need to revise for my quiz.
+    for (var i = 0; i < questions.length; i++) {
+        // Display current question to user and ask OK/Cancel
+
+        // Compare answers
+        if (
+          (answer === true && questions[i].a === 't') ||
+          (answer === false && questions[i].a === 'f')
+        ) {
+          // Increase score
+          score++;
+          alert('Correct!');
+        } else {
+          alert('Wrong!');
+        }
+      }
+
 
 
 
@@ -83,3 +145,9 @@ var display
 
 
 // High score function shows user's final score and initials in a list
+
+
+// Calling function
+
+displayStart();
+
